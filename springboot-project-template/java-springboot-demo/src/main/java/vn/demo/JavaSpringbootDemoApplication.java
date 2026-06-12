49cdd6d82216dbbe10dfe99f2ae8e4e6058e43f3
nguyenvudangkhoa189 <54623905+nguyenvudangkhoa189@users.noreply.github.com>
2026-06-12 22:09:26 +0700

    add spring boot project template

---
+++ springboot-project-template/java-springboot-demo/src/main/java/vn/demo/JavaSpringbootDemoApplication.java
@@ -0,0 +1,12 @@
+package vn.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringbootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringbootDemoApplication.class, args);
+	}
+}
